1f9b0988b6aae2e3bc3d0a7e8a3cb068ebee767e
Angela <100213309+anglerfish05@users.noreply.github.com>
2022-10-06 15:55:19 -0400

    i2c outline

---
+++ animatronics_2023_slave.ino
@@ -0,0 +1,103 @@
+#include <Servo.h>
+#include <Wire.h>
+
+//i2c constants
+String answer = "on";
+byte x = 0;
+
+// motor constants
+Motor motor1(5); // bear head
+Motor motor2(5);
+Motor motor3(5);
+Motor motor4(5);
+Motor motor5(5);
+
+class Motor {
+  Servo servo; 
+  int pos;
+  int increment;
+  int updateInterval;
+  unsigned long lastUpdate;
+}
+
+public:
+  Motor(int interval) {
+    updateInterval = interval;
+    increment = 1;
+  }
+
+void Attach(int pin) {
+  servo.attach(pin);
+}
+
+void Detach() {
+  servo.detach();
+}
+
+void Update() {
+  if ((millis() - lastUpdate) > updateInterval) { // time to update
+      lastUpdate = millis();
+      pos += increment;
+      servo.write(pos);
+      Serial.println(pos);
+      if ((pos >= 100) || (pos <= 0)) { // end of sweep
+        // reverse direction
+        increment = -increment;
+      }
+    }
+}
+
+Motor motor1(5);
+Motor motor2(5);
+Motor motor3(5);
+Motor motor4(5);
+
+void receiveEvent() {
+  x = Wire.read();
+}
+
+void requestEvent() {
+  byte response[2]; 
+
+  for( byte i = 0 ; i < 2 ; i++ ) {
+    response[i] = (byte)answer.charAt(i);
+  }
+  Wire.write(response, sizeof(response));
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  motor1.Attach(4);
+  motor2.Attach(5);
+  motor3.Attach(6);
+  motor4.Attach(7);
+
+  Wire.begin(9);
+  Wire.onRequest(requestEvent);
+  Wire.onReceive(receiveEvent);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if( x == 1 ) {
+    
+  }
+  else if( x == 2 ) {
+    
+  }
+  else if( x == 3 ) {
+    
+  }
+  else if( x == 4 ) {
+    
+  }
+  else if( x == 5 ) {
+    
+  }
+  else if( x == 6 ) {
+    
+  }
+  else {
+    
+  }
+}
