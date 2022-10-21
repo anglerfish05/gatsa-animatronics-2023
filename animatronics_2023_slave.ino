73b614d4f0096e2c3a2a85c3d0aeaec0c3ca16e5
Angela <100213309+anglerfish05@users.noreply.github.com>
2022-10-20 22:02:55 -0400

    10.20.2022
    am i done?

--- animatronics_2023_slave.ino
+++ animatronics_2023_slave.ino
@@ -1,5 +1,12 @@
+#include <FastLED.h>
 #include <Servo.h>
 #include <Wire.h>
+
+// rgb constants
+#define LED_TYPE WS2812B
+CRGB figureLights1[60];
+CRGB figureLights2[60];
+uint8_t gBrightness = 50;
 
 // pneumatics
 int signalPin1 = 8;
@@ -22,64 +29,63 @@ class Motor {
   int increment;
   int updateInterval;
   unsigned long lastUpdate;
-}
 
-public:
-  Motor(int interval) {
-    updateInterval = interval;
-    increment = 1;
+  public:
+    Motor(int interval) {
+      updateInterval = interval;
+      increment = 1;
+    }
+  
+  void Attach(int pin) {
+    servo.attach(pin);
   }
+  
+  void Detach() {
+    servo.detach();
+  }
+  
+  void Update1() {
+    if ((millis() - lastUpdate) > updateInterval) { // time to update
+        lastUpdate = millis();
+        pos += increment;
+        servo.write(pos);
+        Serial.println(pos);
+        if ((pos >= 100) || (pos <= 0)) { // end of sweep
+          // reverse direction
+          increment = -increment;
+        } 
+      }
+  }
+  
+  void Update2() {
+    servo.write(95);
+  }
+};
 
-void Attach(int pin) {
-  servo.attach(pin);
-}
-
-void Detach() {
-  servo.detach();
-}
-
-void Update1() {
-  if ((millis() - lastUpdate) > updateInterval) { // time to update
-      lastUpdate = millis();
-      pos += increment;
-      servo.write(pos);
-      Serial.println(pos);
-      if ((pos >= 100) || (pos <= 0)) { // end of sweep
-        // reverse direction
-        increment = -increment;
-      } 
-    }
-}
-
-void Update2() {
-  if ((millis() - lastUpdate) > updateInterval) { // time to update
-      lastUpdate = millis();
-      pos += increment;
-      servo.write(pos);
-      Serial.println(pos);
-      /* if ((pos >= 100) || (pos <= 0)) { // end of sweep
-        // reverse direction
-        increment = -increment;
-      }  */
-    }
-}
-
-Motor motor1(5);
+Motor motor1(5); // increase # slows down, decrease faster
 Motor motor2(5);
 Motor motor3(5);
 Motor motor4(5);
 
-void receiveEvent() {
-  x = Wire.read();
+void lightsOff() {
+  fill_solid(figureLights1, 60, CRGB::Black);
+  fill_solid(figureLights2, 60, CRGB::Black);
+  FastLED.show();
 }
-
-void requestEvent() {
-  byte response[2]; 
-
-  for( byte i = 0 ; i < 2 ; i++ ) {
-    response[i] = (byte)answer.charAt(i);
-  }
-  Wire.write(response, sizeof(response));
+void lightsOn() {
+  fill_solid(figureLights1, 60, FAF5DF);
+  fill_solid(figureLights2, 60, FAF5DF);
+  FastLED.show();
+}
+void lightsOnL() {
+  fill_solid(figureLights1, 60, FAF5DF);
+  fill_solid(figureLights2, 60, CRGB::Black);
+  FastLED.show();
+}
+void lightsOnR() {
+  fill_solid(figureLights1, 60, CRGB::Black);
+  fill_solid(figureLights2, 60, FAF5DF);
+  FastLED.show();
 }
 
 void setup() {
@@ -95,23 +101,43 @@ void setup() {
   Wire.begin(9);
   Wire.onRequest(requestEvent);
   Wire.onReceive(receiveEvent);
+
+  FastLED,addLeds<LED_TYPE, 2, GRB> (figureLights1, 60).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, 3, GRB> (figureLights2, 60).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(50);
+}
+
+void receiveEvent() {
+  x = Wire.read();
+}
+
+void requestEvent() {
+  byte response[2]; 
+
+  for( byte i = 0 ; i < 2 ; i++ ) {
+    response[i] = (byte)answer.charAt(i);
+  }
+  Wire.write(response, sizeof(response));
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if( x == 1 ) {
+  if( x == 1 ) { // attractions
     motor1.Update2();
   }
-  else if( x == 2 ) {
+  else if( x == 2 ) { // food
     motor2.Update1();
   }
-  else if( x == 3 ) {
+  else if( x == 3 ) { // state parks
+    lightsOnL();
     motor3.Update2();
   }
   else if( x == 4 ) {
+    lightsOnR();
     motor4.Update2();
   }
   else if( x == 5 ) {
+    lightsOn();
     digitalWrite(signalPin1, HIGH);
     digitalWrite(signalPin2, HIGH);
     delay(5000);
