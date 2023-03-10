384ddf7fa98494ce37e526110c93067a0f30acfa
Angela <100213309+anglerfish05@users.noreply.github.com>
2023-03-10 16:55:09 -0500

    Add files via upload

--- animatronics_2023_slave.ino
+++ animatronics_2023_slave.ino
@@ -1,12 +1,12 @@
-#include <FastLED.h>
 #include <Servo.h>
 #include <Wire.h>
 
-// rgb constants
-#define LED_TYPE WS2812B
-CRGB figureLights1[60];
-CRGB figureLights2[60];
-uint8_t gBrightness = 50;
+// motor
+Servo motor;
+int motorControl(int value) {
+  motor.write(map(value, -100, 100, 1000, 2000));
+}
+
 
 // pneumatics
 int signalPin1 = 8;
@@ -16,95 +16,18 @@ int signalPin2 = 9;
 String answer = "on";
 byte x = 0;
 
-// motor constants
-Motor motor1(5); // bear head
-Motor motor2(5);
-Motor motor3(5);
-Motor motor4(5);
-Motor motor5(5);
-
-class Motor {
-  Servo servo; 
-  int pos;
-  int increment;
-  int updateInterval;
-  unsigned long lastUpdate;
-
-  public:
-    Motor(int interval) {
-      updateInterval = interval;
-      increment = 1;
-    }
-  
-  void Attach(int pin) {
-    servo.attach(pin);
-  }
-  
-  void Detach() {
-    servo.detach();
-  }
-  
-  void Update1() {
-    if ((millis() - lastUpdate) > updateInterval) { // time to update
-        lastUpdate = millis();
-        pos += increment;
-        servo.write(pos);
-        Serial.println(pos);
-        if ((pos >= 100) || (pos <= 0)) { // end of sweep
-          // reverse direction
-          increment = -increment;
-        } 
-      }
-  }
-  
-  void Update2() {
-    servo.write(95);
-  }
-};
-
-Motor motor1(5); // increase # slows down, decrease faster
-Motor motor2(5);
-Motor motor3(5);
-Motor motor4(5);
-
-void lightsOff() {
-  fill_solid(figureLights1, 60, CRGB::Black);
-  fill_solid(figureLights2, 60, CRGB::Black);
-  FastLED.show();
-}
-void lightsOn() {
-  fill_solid(figureLights1, 60, FAF5DF);
-  fill_solid(figureLights2, 60, FAF5DF);
-  FastLED.show();
-}
-void lightsOnL() {
-  fill_solid(figureLights1, 60, FAF5DF);
-  fill_solid(figureLights2, 60, CRGB::Black);
-  FastLED.show();
-}
-void lightsOnR() {
-  fill_solid(figureLights1, 60, CRGB::Black);
-  fill_solid(figureLights2, 60, FAF5DF);
-  FastLED.show();
-}
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(4, OUTPUT);
+  motor.attach(4);
+  
   pinMode(signalPin1, OUTPUT);
   pinMode(signalPin2, OUTPUT);
-  
-  motor1.Attach(4);
-  motor2.Attach(5);
-  motor3.Attach(6);
-  motor4.Attach(7);
 
   Wire.begin(9);
   Wire.onRequest(requestEvent);
   Wire.onReceive(receiveEvent);
-
-  FastLED,addLeds<LED_TYPE, 2, GRB> (figureLights1, 60).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<LED_TYPE, 3, GRB> (figureLights2, 60).setCorrection(TypicalLEDStrip);
-  FastLED.setBrightness(50);
 }
 
 void receiveEvent() {
@@ -123,20 +46,15 @@ void requestEvent() {
 void loop() {
   // put your main code here, to run repeatedly:
   if( x == 1 ) { // attractions
-    motor1.Update2();
-  }
-  else if( x == 2 ) { // food
-    motor2.Update1();
+    motorControl(100);
   }
   else if( x == 3 ) { // state parks
-    lightsOnL();
-    motor3.Update2();
+    motorControl(100);
   }
-  else if( x == 4 ) {
-    lightsOnR();
-    motor4.Update2();
+  else if( x == 4 ) { // historical sites
+    motorControl(100);
   }
-  else if( x == 5 ) {
+  else if( x == 5 ) { // historical figures
     lightsOn();
     digitalWrite(signalPin1, HIGH);
     digitalWrite(signalPin2, HIGH);
@@ -144,10 +62,7 @@ void loop() {
     digitalWrite(signalPin1, LOW);
     digitalWrite(signalPin2, LOW);
   }
-  else if( x == 6 ) {
-    
-  }
-  else {
+  else if ( x == 2 ) { // everything off
     
   }
 }
